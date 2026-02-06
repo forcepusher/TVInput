8f99e0f449e0d4093855bfeac58e70a1229ba842
forcepusher <bionitsoup@gmail.com>
2026-02-06 07:46:00 +0700

    Create keys

--- Packages/com.bananaparty.tvinput.web/Runtime/Key.cs
+++ Packages/com.bananaparty.tvinput.web/Runtime/Key.cs
@@ -1,10 +1,12 @@
+using UnityEngine;
+
 namespace BananaParty.TVInput
 {
     public class Key
     {
         private bool _held;
 
-        public Key()
+        public Key(int webKeyCode, KeyCode unityKeyCode)
         {
 
         }
--- Packages/com.bananaparty.tvinput.web/Runtime/TVRemote.cs
+++ Packages/com.bananaparty.tvinput.web/Runtime/TVRemote.cs
@@ -1,7 +1,13 @@
+using UnityEngine;
+
 namespace BananaParty.TVInput
 {
     public class TVRemote
     {
-
+        public readonly Key SubmitKey = new(13, KeyCode.JoystickButton0);
+        public readonly Key LeftKey = new(13, KeyCode.JoystickButton14);
+        public readonly Key RightKey = new(13, KeyCode.JoystickButton15);
+        public readonly Key UpKey = new(13, KeyCode.JoystickButton12);
+        public readonly Key DownKey = new(13, KeyCode.JoystickButton13);
     }
 }
