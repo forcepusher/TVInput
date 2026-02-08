b4752aa6401d17f80ba282bfea715c3e6edda11b
forcepusher <bionitsoup@gmail.com>
2026-02-09 01:51:31 +0700

    Add helper method

--- Packages/com.bananaparty.tvinput.web/Runtime/TVRemote.cs
+++ Packages/com.bananaparty.tvinput.web/Runtime/TVRemote.cs
@@ -4,12 +4,22 @@ namespace BananaParty.TVInput
 {
     public class TVRemote
     {
+        public static bool IsRunningOnWebGL
+        {
+            get
+            {
+#if UNITY_WEBGL && !UNITY_EDITOR
+                return true;
+#else
+                return false;
+#endif
+            }
+        }
+
         public readonly Key SubmitKey = new(KeyCode.JoystickButton0, 13, WebInputSource.Keyboard);
         public readonly Key UpKey = new(KeyCode.JoystickButton12, 12, WebInputSource.Gamepad);
         public readonly Key DownKey = new(KeyCode.JoystickButton13, 13, WebInputSource.Gamepad);
         public readonly Key LeftKey = new(KeyCode.JoystickButton14, 14, WebInputSource.Gamepad);
         public readonly Key RightKey = new(KeyCode.JoystickButton15, 15, WebInputSource.Gamepad);
-        
-        
     }
 }
