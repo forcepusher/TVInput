cc229255bc3858cddbe41b78d7cefac276054392
forcepusher <bionitsoup@gmail.com>
2026-02-10 01:57:00 +0700

    Boilerplate

--- Assets/Scripts/ButtonStateText.cs
+++ Assets/Scripts/ButtonStateText.cs
@@ -9,18 +9,64 @@ namespace BananaParty.Input.TVRemote.Sample
         private EventQueue<PressEvent> _okButtonPressEventQueue;
         private EventQueue<ReleaseEvent> _okButtonReleaseEventQueue;
 
+        private EventQueue<PressEvent> _upButtonPressEventQueue;
+        private EventQueue<ReleaseEvent> _upButtonReleaseEventQueue;
+
+        private EventQueue<PressEvent> _downButtonPressEventQueue;
+        private EventQueue<ReleaseEvent> _downButtonReleaseEventQueue;
+
+        private EventQueue<PressEvent> _leftButtonPressEventQueue;
+        private EventQueue<ReleaseEvent> _leftButtonReleaseEventQueue;
+
+        private EventQueue<PressEvent> _rightButtonPressEventQueue;
+        private EventQueue<ReleaseEvent> _rightButtonReleaseEventQueue;
+
         private string _eventLog;
 
         private void OnEnable()
         {
             _okButtonPressEventQueue = _tvRemote.OkButton.PressEventHub.Subscribe();
             _okButtonReleaseEventQueue = _tvRemote.OkButton.ReleaseEventHub.Subscribe();
+
+            _upButtonPressEventQueue = _tvRemote.UpButton.PressEventHub.Subscribe();
+            _upButtonReleaseEventQueue = _tvRemote.UpButton.ReleaseEventHub.Subscribe();
+
+            _downButtonPressEventQueue = _tvRemote.DownButton.PressEventHub.Subscribe();
+            _downButtonReleaseEventQueue = _tvRemote.DownButton.ReleaseEventHub.Subscribe();
+
+            _leftButtonPressEventQueue = _tvRemote.LeftButton.PressEventHub.Subscribe();
+            _leftButtonReleaseEventQueue = _tvRemote.LeftButton.ReleaseEventHub.Subscribe();
+
+            _rightButtonPressEventQueue = _tvRemote.RightButton.PressEventHub.Subscribe();
+            _rightButtonReleaseEventQueue = _tvRemote.RightButton.ReleaseEventHub.Subscribe();
         }
 
         private void OnDisable()
         {
             _tvRemote.OkButton.PressEventHub.Unsubscribe(_okButtonPressEventQueue);
             _tvRemote.OkButton.ReleaseEventHub.Unsubscribe(_okButtonReleaseEventQueue);
+
+            _tvRemote.UpButton.PressEventHub.Unsubscribe(_upButtonPressEventQueue);
+            _tvRemote.UpButton.ReleaseEventHub.Unsubscribe(_upButtonReleaseEventQueue);
+
+            _tvRemote.DownButton.PressEventHub.Unsubscribe(_downButtonPressEventQueue);
+            _tvRemote.DownButton.ReleaseEventHub.Unsubscribe(_downButtonReleaseEventQueue);
+
+            _tvRemote.LeftButton.PressEventHub.Unsubscribe(_leftButtonPressEventQueue);
+            _tvRemote.LeftButton.ReleaseEventHub.Unsubscribe(_leftButtonReleaseEventQueue);
+
+            _tvRemote.RightButton.PressEventHub.Unsubscribe(_rightButtonPressEventQueue);
+            _tvRemote.RightButton.ReleaseEventHub.Unsubscribe(_rightButtonReleaseEventQueue);
+        }
+
+        // Yes, FixedUpdate with input is intentional
+        private void FixedUpdate()
+        {
+            while (_okButtonPressEventQueue.HasUnreadEvents)
+                _eventLog = $"{nameof(_tvRemote.OkButton)} press at {_okButtonPressEventQueue.Read().Time}\n" + _eventLog;
+
+            while (_okButtonReleaseEventQueue.HasUnreadEvents)
+                _eventLog = $"{nameof(_tvRemote.OkButton)} release at {_okButtonPressEventQueue.Read().Time}\n" + _eventLog;
         }
     }
 }
