ba262cca8f4819ef3a9301424686347bc61eb6a3
forcepusher <bionitsoup@gmail.com>
2026-02-10 01:28:51 +0700

    Rename

--- Assets/Scripts/KeyStateText.cs
+++ Assets/Scripts/KeyStateText.cs
@@ -4,6 +4,18 @@ namespace BananaParty.TVInput.Sample
 {
     public class KeyStateText : MonoBehaviour
     {
+        private TVRemote _tvRemote = new();
 
+        private string _text;
+
+        private void OnEnable()
+        {
+            //_tvRemote.DownKey
+        }
+
+        private void OnDisable()
+        {
+            
+        }
     }
 }
