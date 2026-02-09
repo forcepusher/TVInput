74b0ec3e402a02d1913bccf9eb8d40bb7d9f8e1a
forcepusher <bionitsoup@gmail.com>
2026-02-10 03:45:34 +0700

    Match constructor

--- Packages/com.bananaparty.input.tvremote/Runtime/TVRemote.cs
+++ Packages/com.bananaparty.input.tvremote/Runtime/TVRemote.cs
@@ -4,11 +4,11 @@ namespace BananaParty.Input.TVRemote
 {
     public class TVRemote
     {
-        public readonly TVRemoteButton OkButton = new(KeyCode.JoystickButton0, 13, WebInputDeviceType.Keyboard);
-        public readonly TVRemoteButton UpButton = new(KeyCode.JoystickButton12, 12, WebInputDeviceType.Gamepad);
-        public readonly TVRemoteButton DownButton = new(KeyCode.JoystickButton13, 13, WebInputDeviceType.Gamepad);
-        public readonly TVRemoteButton LeftButton = new(KeyCode.JoystickButton14, 14, WebInputDeviceType.Gamepad);
-        public readonly TVRemoteButton RightButton = new(KeyCode.JoystickButton15, 15, WebInputDeviceType.Gamepad);
+        public readonly TVRemoteButton OkButton = new(WebInputDeviceType.Keyboard, 13, KeyCode.JoystickButton0);
+        public readonly TVRemoteButton UpButton = new(WebInputDeviceType.Gamepad, 12, KeyCode.JoystickButton12);
+        public readonly TVRemoteButton DownButton = new(WebInputDeviceType.Gamepad, 13, KeyCode.JoystickButton13);
+        public readonly TVRemoteButton LeftButton = new(WebInputDeviceType.Gamepad, 14, KeyCode.JoystickButton14);
+        public readonly TVRemoteButton RightButton = new(WebInputDeviceType.Gamepad, 15, KeyCode.JoystickButton15);
 
         public void PollInput()
         {
