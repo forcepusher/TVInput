a4a8bf2b667ceaef8faa4128ce5089a5653a6851
forcepusher <bionitsoup@gmail.com>
2026-02-11 16:40:00 +0700

    Boilerplate

--- Packages/com.bananaparty.input.tvremote/Runtime/TVRemoteButton.cs
+++ Packages/com.bananaparty.input.tvremote/Runtime/TVRemoteButton.cs
@@ -12,9 +12,6 @@ namespace BananaParty.Input.TVRemote
         public readonly EventHub<PressEvent> PressEventHub = new();
         public readonly EventHub<ReleaseEvent> ReleaseEventHub = new();
 
-        //private int _pressCount;
-        //private int _releaseCount;
-
         public bool IsHeld { get; private set; }
 
         public TVRemoteButton(WebInputDeviceType webInputDeviceType, int webKeyCode, KeyCode unityKeyCode)
@@ -25,9 +22,6 @@ namespace BananaParty.Input.TVRemote
 
             if (IsRunningOnWeb)
                 WebInputBridge.RegisterButton(webInputDeviceType, webKeyCode);
-
-            //if (TVRemote.IsRunningOnWeb)
-            //    KeyInitialize(webKeyIndex, (int)webInputSource);
         }
 
         public static bool IsRunningOnWeb
@@ -44,90 +38,11 @@ namespace BananaParty.Input.TVRemote
 
         public void PollInput()
         {
-            while (WebInputBridge.ReadPressEventsForKey())
-                PressEventHub.AddEvent(new PressEvent(Time.realtimeSinceStartup));
+            while (WebInputBridge.HasUnreadPressEvents(_webInputDeviceType, _webKeyCode))
+                PressEventHub.AddEvent(WebInputBridge.ReadPressEvents(_webInputDeviceType, _webKeyCode));
 
-            while (WebInputBridge.ReadReleaseEventsForKey())
-                PressEventHub.AddEvent(new ReleaseEvent(Time.realtimeSinceStartup));
+            while (WebInputBridge.HasUnreadReleaseEvents(_webInputDeviceType, _webKeyCode))
+                ReleaseEventHub.AddEvent(WebInputBridge.ReadReleaseEvents(_webInputDeviceType, _webKeyCode));
         }
-
-        //[DllImport("__Internal")]
-        //private static extern bool KeyInitialize(int webKeyIndex, int webInputSource);
-
-        //public bool IsHeld
-        //{
-        //    get
-        //    {
-        //        if (TVRemote.IsRunningOnWeb)
-        //            return GetKeyIsHeld(_webKeyIndex, (int)_webInputSource);
-        //        else
-        //            return Input.GetKey(_unityKeyCode);
-        //    }
-        //}
-
-        //public void PollInput()
-        //{
-        //    _pressCount += Input.GetKeyDown(_unityKeyCode) ? 1 : 0;
-        //    _releaseCount += Input.GetKeyUp(_unityKeyCode) ? 1 : 0;
-        //}
-
-        //[DllImport("__Internal")]
-        //private static extern bool GetKeyIsHeld(int webKeyIndex, int webInputSource);
-
-        //public int ConsumePresses(int webKeyIndex, WebInputDevice webInputSource)
-        //{
-        //    if (TVRemote.IsRunningOnWeb)
-        //    {
-        //        return KeyConsumePresses(webKeyIndex, (int)webInputSource);
-        //    }
-        //    else
-        //    {
-        //        var presses = _pressCount;
-        //        _pressCount = 0;
-        //        return presses;
-        //    }
-        //}
-
-        //[DllImport("__Internal")]
-        //private static extern int KeyConsumePresses(int webKeyIndex, int webInputSource);
-
-        //public int PeekPresses(int webKeyIndex, WebInputDevice webInputSource)
-        //{
-        //    if (TVRemote.IsRunningOnWeb)
-        //        return KeyPeekPresses(webKeyIndex, (int)webInputSource);
-        //    else
-        //        return _pressCount;
-        //}
-
-        //[DllImport("__Internal")]
-        //private static extern int KeyPeekPresses(int webKeyIndex, int webInputSource);
-
-        //public int ConsumeReleases(int webKeyIndex, WebInputDevice webInputSource)
-        //{
-        //    if (TVRemote.IsRunningOnWeb)
-        //    {
-        //        return KeyConsumeReleases(webKeyIndex, (int)webInputSource);
-        //    }
-        //    else
-        //    {
-        //        var releases = _releaseCount;
-        //        _releaseCount = 0;
-        //        return releases;
-        //    }
-        //}
-
-        //[DllImport("__Internal")]
-        //private static extern int KeyConsumeReleases(int webKeyIndex, int webInputSource);
-
-        //public int PeekReleases(int webKeyIndex, WebInputDevice webInputSource)
-        //{
-        //    if (TVRemote.IsRunningOnWeb)
-        //        return KeyPeekReleases(webKeyIndex, (int)webInputSource);
-        //    else
-        //        return _releaseCount;
-        //}
-
-        //[DllImport("__Internal")]
-        //private static extern int KeyPeekReleases(int webKeyIndex, int webInputSource);
     }
 }
